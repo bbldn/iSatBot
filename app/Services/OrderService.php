238308f8726a5126857cea037681fafcd58b7fe9
Boris Bobol <bobol.boris@gmail.com>
2020-06-11 23:32:09 +0300

    11.06.2020 23:32

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -13,16 +13,21 @@ class OrderService extends Service
     public function getOrderInformation(int $id): array
     {
         $response = file_get_contents(env('API_URL') . '/api/order/back/information/' . $id);
+
+        if (false === $response) {
+            return ['Сервер недоступен'];
+        }
+
         $response = json_encode($response);
 
         if (false === $response) {
-            return ['Сервер вернул ошибку'];
+            return ['Сервер вернул не правильный ответ'];
         }
 
         if (false === $response['ok']) {
-            return ['Сервер вернул ошибку'];
+            return ['Сервер вернул ошибку:'];
         }
 
-        return OrderFormatter::formatOrder($response);
+        return OrderFormatter::formatOrder($response['data']);
     }
 }
