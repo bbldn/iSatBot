7f3ad6b3da2a178bb6146efa1fcd73bb68757e3b
Boris Bobol <bobol.boris@gmail.com>
2020-07-06 21:46:07 +0300

    06.07.2020 21:45

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,3 @@ Route::post('/telegram/webook', 'TelegramController@webHookHandlerAction');
 
 Route::get('/order/new', 'TelegramController@newOrderNotifyAction');
 Route::post('/order/new', 'TelegramController@newOrderNotifyAction');
-//
