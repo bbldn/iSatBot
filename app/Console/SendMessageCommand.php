239a15475696af63c2482c26a78ed093479e4540
Boris Bobol <bobol.boris@gmail.com>
2020-07-15 21:31:06 +0300

    15.07.2020 21:31

---
+++ app/Console/SendMessageCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console;
+
+use App\Chat;
+use App\Events\EventList;
+use App\Helpers\EventListeners;
+use Illuminate\Console\Command;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class SendMessageCommand extends Command
+{
+    /** @var string $signature */
+    protected $signature = 'telegram:message:send {text}';
+
+    /** @var string $description */
+    protected $description = 'Send Message';
+
+    /**
+     *
+     */
+    public function handle(): void
+    {
+        $chats = EventListeners::getChatsByEvent(EventList::ORDER_NEW);
+
+        foreach ($chats as $chat) {
+            /** @noinspection PhpUndefinedMethodInspection */
+            /** @var Chat $chat */
+            Telegram::sendDocument([
+                'chat_id' => $chat->chat_id,
+                'text' => $this->argument('text'),
+            ]);
+        }
+    }
+}
