dcb4a2a34c0a3b564052f6041e4c403fe4f7a475
Boris Bobol <bobol.boris@gmail.com>
2020-08-13 14:51:36 +0300

    13.08.2020 14:12

--- resources/views/cheaper.blade.php
+++ resources/views/cheaper.blade.php
@@ -1,4 +1,9 @@
 <strong>Нашли дешевле</strong>
+
 @foreach($data as $value)
+    @if (filter_var($value['value'], FILTER_VALIDATE_URL))
+{{ $value['name'] }}: <strong><a href="{{ $value['value'] }}">Перейти</a></strong>
+    @else
 {{ $value['name'] }}: <strong>{{ $value['value'] }}</strong>
+    @endif
 @endforeach
