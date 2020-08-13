9af75c8cfa4a820a3d48fedabcc2b884157bc76e
Boris Bobol <bobol.boris@gmail.com>
2020-08-13 15:11:02 +0300

    13.08.2020 15:10

--- resources/views/cheaper.blade.php
+++ resources/views/cheaper.blade.php
@@ -3,6 +3,8 @@
 @foreach($data as $value)
 @if (filter_var($value['value'], FILTER_VALIDATE_URL))
 {{ $value['name'] }}: <strong><a href="{{ $value['value'] }}">Перейти</a></strong>
+@elseif (1 === preg_match('/^38 \([0-9]{3}\) [0-9]{3}-[0-9]{2}-[0-9]{2}$/', $value['value']))
+{{ $value['name'] }}: <strong><a href="{{ $value['value'] }}">{{ $value['value'] }}</a></strong>
 @else
 {{ $value['name'] }}: <strong>{{ $value['value'] }}</strong>
 @endif
